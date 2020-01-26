8c02e943b2f5d0a748e2b36750491f619360d8c8
fuziyu <fuziyu@dxy.cn>
2020-01-26 22:42:39 +0800

    [fix] 增加title数据，更新代码

--- demo_ahocorasick.py
+++ demo_ahocorasick.py
@@ -5,7 +5,10 @@ from tire import ACTree
 from tire import remove_contain
 
 # 词典文件 key 为类别，value为文件地址
-dict_files = {'district':'./data/district_dict', 'category': './data/category_dict'}
+dict_files = {'district':'./data/district_dict',
+              'category': './data/category_dict',
+              'crowd': './data/crowd_dict',
+              'count': './data/count_dict'}
 
 # 分句子模版
 split_pattern = re.compile('[。|；|;|,|，|、|（|）|\(|\)]')
@@ -41,53 +44,80 @@ def get_people_pattern(content):
     people_size = people_pattern.findall(content)
     return people_size
 
+
+def get_struct_data(ac, title, content):
+    title = title.strip()
+    content = content.replace('\n', '')
+    title_res, title_district = get_sentence(ac, title, istitle=True)
+    content_res, title_district = get_sentence(ac, content, istitle=False, title_district=title_district)
+
+    return title_res + content_res
+
+
+def init_category(entitys, sentence_len):
+    for e in entitys:
+        if e['tag'] == 'category':
+            return ':'.join([e['name'], str(e['start'] + sentence_len), str(e['end'] + sentence_len)])
+    return '确诊'
+
+def get_sentence(ac, sentence, istitle=False, title_district=None):
+    res = []
+    category = None
+    district = None
+    count = '累计'
+
+    # 将长句分为短句
+    span = split_pattern.split(sentence)
+
+    sentence_len = 0
+
+    for index, j in enumerate(span):
+
+        crowd = None
+
+        # 获取每一个短句中存在的人数信息，并将人数信息加入到字典树中。
+        s = set(get_people_pattern(j.strip()))
+        people_size_entity = [[t, 'size'] for t in s]
+        ac.add_entity(people_size_entity)
+
+        # 最长匹配所有实体，无重叠
+        entity = remove_contain(ac.search_ner(j))
+        if not category or ':' not in category:
+            category = init_category(entity, sentence_len)
+
+        for e in entity:
+            # 更新地点数据
+            if e['tag'] == 'district':
+                district = ':'.join([e['name'], str(e['start'] + sentence_len), str(e['end'] + sentence_len)])
+                if istitle and not title_district:
+                    title_district = e['name']
+            if e['tag'] == 'count':
+                count = ':'.join([e['name'], str(e['start'] + sentence_len), str(e['end'] + sentence_len)])
+            if e['tag'] == 'crowd':
+                crowd = ':'.join([e['name'], str(e['start'] + sentence_len), str(e['end'] + sentence_len)])
+            # 更新类别数据
+            if e['tag'] == 'category':
+                category = ':'.join([e['name'], str(e['start'] + sentence_len), str(e['end'] + sentence_len)])
+            # 对每一个抽取到的人口信息，给定地点和类别信息，并判断是增量还是累计值
+            if e['tag'] == 'size' and district and category:
+                res.append([sentence, j, count, crowd, istitle, title_district, district, category, e['name']])
+        sentence_len += len(j)
+    return res, title_district
+
+
 if __name__ == '__main__':
 
     # 初始化字典树
     ac = get_type_tire(dict_files)
 
     res = []
+    titles = open('./data/raw_title.txt').read().split('\n')
+    contents = open('./data/raw_content.txt').read().split('\n')
 
-    with open('./data/content_data') as data:
-
-        for i in data:
-
-            # 初始化信息，严格控制地点数据。
-            category = '确诊'
-            district = None
-            i_type = '累计'
-
-            # 将长句分为短句
-            small_sentences = split_pattern.split(i)
-
-            for j in small_sentences:
-                if '新增' in j:
-                    i_type = '新增'
-                if '累计' in j or '增至' in j:
-                    i_type = '累计'
-                print(j)
-
-                # 获取每一个短句中存在的人数信息，并将人数信息加入到字典树中。
-                s = set(get_people_pattern(j.strip()))
-                people_size_entity = [[t, 'size'] for t in s]
-                ac.add_entity(people_size_entity)
-
-                # 最长匹配所有实体，无重叠
-                entity = remove_contain(ac.search_ner(j))
-                for e in entity:
-                    # 更新地点数据
-                    if e['tag'] == 'district':
-                        district = e['name']
-                    # 更新类别数据
-                    if e['tag'] == 'category':
-                        category = e['name']
-                    # 对每一个抽取到的人口信息，给定地点和类别信息，并判断是增量还是累计值
-                    if e['tag'] == 'size' and district and category:
-                        res.append([i, j, district, category, e['name'], i_type])
-
-                    # print(e)
+    for title, content in zip(titles, contents):
+        res.extend(get_struct_data(ac, title, content))
 
 
-    data = pd.DataFrame(res, columns=['content', 'small_content', '地点', '类别', '人数', '+？'])
+    data = pd.DataFrame(res, columns=['content', 'span', 'count', '特殊人群', 'istitle', '标题地点', '地点', '类别', '人数'])
 
     data.to_csv('./data/res.csv', index=False)
